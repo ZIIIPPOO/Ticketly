e0ccd0a1766acd0975fcebc469d451a12db87fa4
ZIIIPPOO <youssefboudouar77@gmail.com>
2025-10-30 17:03:00 +0100

    Added 1st section's content

--- ticketly.js
+++ ticketly.js
@@ -1,4 +1,30 @@
 function affichage(index){
-    document.getElementById('s2').style.display='block';
-    document.getElementById('s1').style.display='none';
+    if(index == 1)
+    {
+        document.getElementById('s2').style.display='block';
+        document.getElementById('s1').style.display='none';
+    }
+    else
+    {
+        document.getElementById('s2').style.display='none';
+        document.getElementById('s1').style.display='block';
+    }
+}
+let counteur =0
+function ctr() {
+    if(counteur < 500){
+    counteur++
+    document.getElementById('ctrn').innerHTML=counteur
+    }
+}
+function decr() {
+    if(counteur > 1)
+    {
+    counteur--;
+    document.getElementById('ctrn').innerHTML=counteur
+    }
+}
+let store;
+function store(index){
+
 }
