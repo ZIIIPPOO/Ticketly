a18e7b6aac85256bb59ca037bc443b435820676c
ZIIIPPOO <youssefboudouar77@gmail.com>
2025-10-31 12:47:51 +0100

    Added tickets number manipulation

--- ticketly.js
+++ ticketly.js
@@ -1,3 +1,4 @@
+document.getElementById('button').disabled = true;
 function affichage(i){
     if(i == 1)
     {
@@ -20,9 +21,29 @@ function affichage(i){
         document.getElementById('s2').style.display='block';
     }
 }
+let new_price = 0;
+let nb_of_seats = 0;
+function store(button){
+    const div = button.parentElement;
+    
+    const title = div.querySelector('h2').innerHTML;
+    const date = div.querySelector('.infos p:nth-child(1)').innerHTML;
+    const location = div.querySelector('.infos p:nth-child(2)').innerHTML;
+    const seats = div.querySelector('.infos p:nth-child(3)').innerHTML;
+    const price = div.querySelector('.infos .price').innerHTML;
+    const s = div.querySelector('.infos .seats span').innerHTML;
+    nb_of_seats = s;
+    new_price = parseInt(price);
+
+    document.querySelector('.selected-event h2').innerHTML = title;
+    document.querySelector('.selected-event div p').innerHTML = date + '<br>' + location + '<br>' + seats + '<br>' + price;
+    document.querySelector('.total-price p:nth-child(2)').innerHTML = price;
+    
+}
 let counteur =1;
 function ctr() {
-    if(counteur < 500){
+    console.log(nb_of_seats);
+    if(counteur < nb_of_seats){
     counteur++
     document.getElementById('ctrn').innerHTML=counteur
     }
@@ -35,21 +56,7 @@ function decr() {
     increment_price();
     }
 }
-let new_price = 0;
-function store(button){
-    const div = button.parentElement;
-    
-    const title = div.querySelector('h2').innerHTML;
-    const date = div.querySelector('.infos p:nth-child(1)').innerHTML;
-    const location = div.querySelector('.infos p:nth-child(2)').innerHTML;
-    const seats = div.querySelector('.infos p:nth-child(3)').innerHTML;
-    const price = div.querySelector('.infos .price').innerHTML;
-    new_price = parseInt(price);
-    document.querySelector('.selected-event h2').innerHTML = title;
-    document.querySelector('.selected-event div p').innerHTML = date + '<br>' + location + '<br>' + seats + '<br>' + price;
-    document.querySelector('.total-price p:nth-child(2)').innerHTML = price;
-    
-}
+
 
 function increment_price(){
     let total = counteur * new_price;
@@ -72,4 +79,10 @@ function store_input(){
 function remove()
 {
     document.getElementById('pl').style.display='none';
+}
+
+function reset()
+{
+    document.getElementById('ctrn').innerHTML = 1;
+    counteur = 1;
 }
